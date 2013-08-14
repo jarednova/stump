614bb716d072e78b920770629979106ae610a9ee
Jared Novack <jarednova@upstatement.com>
2013-08-13 23:46:50 -0400

    Initial Commit for BostonBuilt workshop

---
+++ config.rb
@@ -0,0 +1,16 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/wp-content/themes/techtax"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+fonts_dir = "css/fonts"
+relative_assets = true
+
+line_comments = true
+output_style = :compressed 
+# output_style = :compact :compressed :nested :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
